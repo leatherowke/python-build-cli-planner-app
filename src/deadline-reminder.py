218deb384f8d6a9795db687f0e58a3d42493c9d2
leatherowke <103121924+leatherowke@users.noreply.github.com>
2022-10-27 21:29:07 -0500

    started 2.3

---
+++ src/deadline-reminder.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta, abstractmethod
+from _collections_abc import Iterable
+from abc import ABC
+
+class Deadline_reminder():
+    Iterable(ABCMeta)
+
+    def is_due():
+        pass
+    abstractmethod
+
+class DeadLineReminder(ABC, Iterable):
+    def is_due():
+        pass
+    abstractmethod
+
